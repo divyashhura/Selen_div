b18fd768ede45af18f8301e077928669682c6e00
DivyashSinghHura <divyash.hura@infostride.com>
2022-01-13 19:43:21 +0530

    open Chrome using webdriver manager

---
+++ WebDriverManagerDemo.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManagerDemo {
+
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        // Navigate to the google website
+        driver.get("https://www.google.com");
+
+        //driver.quit();
+
+    }
+
+}
