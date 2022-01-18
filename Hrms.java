e9a3e03b27d2b45f5e4c885177acb4e56c943fa3
DivyashSinghHura <divyash.hura@infostride.com>
2022-01-18 18:58:28 +0530

    Automate login,add user and verify.

--- Hrms.java
+++ Hrms.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.zeromq.ZThread;
 
 public class Hrms {
 
